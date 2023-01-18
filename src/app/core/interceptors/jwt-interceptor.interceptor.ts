04d31c55ad7a010cd54b876b91fcd30507adc8f2
KonradDzieciol99 <konradd990212@gmail.com>
2023-01-19 00:15:18 +0100

    init

--- src/app/core/interceptors/jwt-interceptor.interceptor.ts
+++ src/app/core/interceptors/jwt-interceptor.interceptor.ts
@@ -8,28 +8,35 @@ import {
 import { delay, finalize, mergeMap, Observable, take } from 'rxjs';
 import { AuthenticationService } from 'src/app/authentication/authentication.service';
 import { BusyService } from '../services/busy.service';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Injectable()
 export class JwtInterceptorInterceptor implements HttpInterceptor {
 
-  constructor(private busyService: BusyService) {}
+  constructor(private busyService: BusyService,private auth0Service: AuthService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    // if (request.method === 'POST') {
-    //   return next.handle(request);
-    // }
-    // if (request.method === 'DELETE') {
-    //   return next.handle(request);
-    // }
-    // if (request.method === 'PUT') {
-    //   return next.handle(request);
-    // }
-    this.busyService.busy();
-    return next.handle(request).pipe(
-      delay(250),
-      finalize(() => {
-        this.busyService.idle();
+    
+    return this.auth0Service.getAccessTokenSilently().pipe(
+      mergeMap(token => {
+        const tokenReq = request.clone({
+          setHeaders: { Authorization: `Bearer ${token}` }
+        });
+        return next.handle(tokenReq);
       })
     );
+    //return next.handle(request);
+
+    // const token = this.auth0Service.
+
+    // if (token) {
+    //   request = request.clone({
+    //     setHeaders: {
+    //       Authorization: `Bearer ${token}`
+    //     }
+    //   })
+    // }
+
+    //return next.handle(request);
   }
 }
